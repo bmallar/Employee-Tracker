bcd65d86cf636dff8b1b230ba4d0ee3e0bffe4a7
bmallar <benmallar@gmail.com>
2024-04-03 19:29:07 -0500

    everything except updating an employees role

--- db/seeds.sql
+++ db/seeds.sql
@@ -6,22 +6,21 @@ VALUES('It'),
 ('Operator'),    
 ('Lead');
 
-INSERT INTO roles (title, salary, department_id)
+INSERT INTO roles(title, salary, department_id)
 VALUES('Center Field', 500000, 2), 
 ('Left Field', 79832714, 4),
 ('Pitcher', 57484, 5),
 ('Catcher', 182893, 3),
 ('First Base', 1627222, 2),
-('Runner', 189, 6),
+('Runner', 189, 6);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
-VALUES ("Bob", "Bobson", 1, null ), 
-("Ben", "Mallar", 2, 1), 
-("Barry", "Bonds", 1, 1),
-("Hank", "Hill", 5, 1),
-("Ricky", "Spanish", 3, 1),
-("Nikko", "Malilo", 6, 1), 
-("Bill", "Boldy", 4, 1),
-("Han", "Hilljab", 3, 1),
-("Rico", "Nasty", 2, 1),
-
+INSERT INTO employees(first_name, last_name, role_id, manager_id)
+VALUES ('Bob', 'Bobson', 1, null ), 
+('Ben', 'Mallar', 2, 1), 
+('Barry', 'Bonds', 1, 1),
+('Hank', 'Hill', 5, 1),
+('Ricky', 'Spanish', 3, 1),
+('Nikko', 'Malilo', 6, 1), 
+('Bill', 'Boldy', 4, 1),
+('Han', 'Hilljab', 3, 1),
+('Rico', 'Nasty', 2, 1);
--- server.js
+++ server.js
@@ -6,55 +6,71 @@ const cTable = require('console.table')
 //connect to database
 const pool = new Pool(
     {
-      // TODO: Enter PostgreSQL username
-      user: 'postgres',
-      // TODO: Enter PostgreSQL password
-      password: 'Benzo_115',
-      host: 'localhost',
-      database: 'tracker_db'
+        // TODO: Enter PostgreSQL username
+        user: 'postgres',
+        // TODO: Enter PostgreSQL password
+        password: 'Benzo_115',
+        host: 'localhost',
+        database: 'tracker_db'
     },
     console.log(`Connected to the tracker_db database.`)
-  )
-  
-  pool.connect();
-function start(){
-  inquirer
-  .prompt([
-    {
-        type: 'list',
-        name: 'title',
-        message:'',
-        choices: ['View Departments', "View Roles", "Add Department", "Add Role"]
-    },
-]).then(answer => {
-    switch(answer.title){
-        case "View Departments":
-            viewDepartments()
-           break; 
-        case "View Roles":
-            viewRoles()
-           break;
-        case "Add Department":
-            addDepartment()
-            break;
-        case "Add Role":
-            addRole()
-            break;
-    }
-    
-})
-}
-  
+)
 
-function viewDepartments(){
+pool.connect();
+function start() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'title',
+                message: '',
+                choices: ['View Departments', "View Roles", "View Employee", "Add Department", "Add Role", "Add Employee", "Update Employee Role"]
+            },
+        ]).then(answer => {
+            switch (answer.title) {
+                case "View Departments":
+                    viewDepartments()
+                    break;
+                case "View Roles":
+                    viewRoles()
+                    break;
+                case "View Employee":
+                    viewEmployee()
+                    break;
+                case "Add Department":
+                    addDepartment()
+                    break;
+                case "Add Role":
+                    addRole()
+                    break;
+                case "Add Employee":
+                    addEmployee()
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole()
+                    break;
+            }
+
+        })
+}
+
+
+function viewDepartments() {
     pool.query("SELECT * FROM departments", (err, res) => {
         if (err) throw err;
         console.table(res.rows)
     })
 }
 
-function viewRoles(){
+function viewRoles() {
     pool.query("SELECT * FROM roles", (err, res) => {
+        if (err) throw err;
+        console.table(res.rows)
+    })
+}
+
+function viewEmployee() {
+    pool.query("SELECT * FROM employees", (err, res) => {
         if (err) throw err;
         console.table(res.rows)
     })
@@ -63,20 +79,20 @@ function viewRoles(){
 function addDepartment() {
     inquirer.prompt([
         {
-        type: 'input',
-        name: 'depName',
-        message: 'What department would you like to add?'
+            type: 'input',
+            name: 'depName',
+            message: 'What department would you like to add?'
         }
     ])
-    .then(answer => {
-        pool.query ("INSERT INTO departments (department_name) VALUES ($1)", [answer.depName])
-        start()
-    })
+        .then(answer => {
+            pool.query("INSERT INTO departments (department_name) VALUES ($1)", [answer.depName])
+            start()
+        })
 
 }
 
-function addRole(){
-    pool.query("SELECT * FROM departments", (err, res)=>{
+function addRole() {
+    pool.query("SELECT * FROM departments", (err, res) => {
         if (err) throw err
 
         inquirer.prompt([
@@ -96,20 +112,65 @@ function addRole(){
                 message: 'What is the department for the new role?',
                 choices: res.rows.map(department => department.department_name)
             }
-        ]).then(answer => { 
+        ]).then(answer => {
             const departmentName = res.rows.find(department => department.department_name === answer.depId)
 
 
-            pool.query("INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)", 
-            [answer.newTitle, answer.salary, departmentName.id],
-            function(err){
-                console.log(err)
-            })
-            
+            pool.query("INSERT INTO roles (title, salary, department_id) VALUES ($1, $2, $3)",
+                [answer.newTitle, answer.salary, departmentName.id],
+                function (err) {
+                    console.log(err)
+                })
+
             start()
         })
     })
 }
+
+function addEmployee() {
+    pool.query("SELECT * FROM roles", (err, res) => {
+        if (err) throw err
+
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'Please enter employee first name'
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Please enter employee last name'
+            },
+
+
+            {
+                type: 'list',
+                name: 'roleId',
+                message: 'What is the role for the new employee?',
+                choices: res.rows.map(roles => roles.title)
+            }
+
+        ]).then(answer => {
+            const roleName = res.rows.find(role => role.title === answer.roleId)
+
+            pool.query("INSERT INTO employees (first_name, last_name, role_id) VALUES ($1, $2, $3)",
+                [answer.first_name, answer.last_name, roleName.id],
+                function (err) {
+                    console.log(err)
+                })
+            start()
+
+        })
+
+    })
+}
+
+// function updateEmployeeRole () {
+//     pool.query(SELECT)
+// }
+
+
 
 
 
